aaf99db6d62daa410bd1926d52a1c0c57dfd5ad5
abhishek <abh9204@gmail.com>
2025-04-29 14:26:42 +0530

    multidiemntional array

---
+++ eclipse-workspace/array1/src/main/java/com/array/array1/Application.java
@@ -0,0 +1,13 @@
+package com.array.array1;
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+		int[]nums;
+		nums=new int[5];
+		for(int values:nums) {
+			System.out.println(nums[values]);
+		}
+	}
+}
